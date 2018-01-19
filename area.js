7521f837e9d2427b9bb7af795293e8ac519b8f3c
stefanobaggetto <stefano.baggetto@studenti.unitn.it>
2018-01-19 18:07:40 +0100

    risolto conflitto

--- area.js
+++ area.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function getArea(arr){
 	
 	if ((Number.isInteger(arr[0]) && arr[0] >= 0) && 
@@ -8,7 +7,6 @@ function getArea(arr){
 	else{
 		return -1;
 	}
->>>>>>> 2159b974ae2b84c5f858d1d8c3c041e1b6261381
 }
 
 module.exports = getArea;
